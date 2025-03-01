28c93ba56cbdd26cf3e3950b8e2c28128829c35e
Carlos <carlos_aperdomo@soy.sena.edu.co>
2025-03-01 11:29:16 -0500

    se instala talwind version 3 y se prueban estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 class="text-3xl font-bold underline bg-primary">
+        Hello world!
+      </h1>
+    </div>
   )
 }
