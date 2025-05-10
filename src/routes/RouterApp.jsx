2336a09e90f0d1e8d5daa2d948c74186bfb27e03
JEGonDev <juanestebangonzalezcaro567@gmail.com>
2025-05-10 10:03:20 -0500

    refactor(Routes): Se corrigen errores de importacin en el router app

--- src/routes/RouterApp.jsx
+++ src/routes/RouterApp.jsx
@@ -9,24 +9,18 @@ import { ForgotPasswordPage } from "../features/authentication/pages/ForgotPassw
 import { ResetPasswordPage } from "../features/authentication/pages/ResetPasswordPage";
 // Importaciones del modulo de comunidad
 import { CommunityPage } from "../features/community/pages/CommunityPage";
-// Importaciones de módulo educativo
+// Importaciones de modulo educativo
 import { EducationPage } from '../pages/EducationPage';
-import { ModuleDetailPage } from '../features/education/pages/ModuleDetailPage';
-import { AdminModulesPage } from '../features/education/pages/AdminModulesPage'; 
-import { VideoManagementActions } from '../features/education/pages/VideoManagementActions';
-import { DeleteModulePage } from '../features/education/pages/DeleteModulePage';
-import { EditModulePage } from '../features/education/pages/EditModulePage';
-import { CreateModulePage } from '../features/education/pages/CreateModulePage';
-// Importaciones de módulo de monitoreo
+import { OpenModulePage } from '../features/education/pages/OpenModulePage';
+import { ModuleFormPage } from '../features/education/pages/ModuleFormPage';
+// Importaciones del modulo de perfil
+import { ProfileAdminPage } from '../features/profile/pages/ProfileAdminPage';
+import { ProfileEditPage } from '../features/profile/pages/ProfileEditPage';
+// Importaciones del modulo de Monitoreo
 import { MonitoringPage } from '../pages/MonitoringPage';
 import { RealTimeMonitoringPage } from '../features/crops/pages/RealTimeMonitoringPage';
 import { AlertsPage } from '../features/crops/pages/AlertsPage';
 import { DataHistoryPage } from '../features/crops/pages/DataHistoryPage';
-import { ProfileAdminPage } from '../features/profile/pages/ProfileAdminPage';
-import { ProfileEditPage } from '../features/profile/pages/ProfileEditPage';
-
-
-
 
 
 export const RouterApp = () => {
@@ -46,17 +40,9 @@ export const RouterApp = () => {
 
       {/* Rutas del modulo educativo */}
       <Route path="/education" element={<EducationPage />} />
-      <Route path="/education/module" element={<ModuleDetailPage />} />
-      <Route path="/education/moduleAdmin" element={<AdminModulesPage />} />
-      <Route path="/education/managementActions" element={<VideoManagementActions/>} />
-      <Route path="/education/adminDelete" element={<DeleteModulePage/>} />
-      <Route path="/education/adminEditPage" element={<EditModulePage/>} />
-      <Route path="/education/adminCreateModule" element={<CreateModulePage/>} />
-     {/* Rutas del modulo de monitoreo */}
-      <Route path="/monitoring" element={<MonitoringPage />} />
-      <Route path="/monitoring/history" element={<DataHistoryPage />} />
-      <Route path="/monitoring/alerts" element={<AlertsPage />} />
-      <Route path="/monitoring/real-time" element={<RealTimeMonitoringPage />} />
+      <Route path="/education/module/:moduleId" element={<OpenModulePage />} />
+      <Route path="/education/module-form/:moduleId?" element={<ModuleFormPage />} />
+
       {/* Rutas del modulo de comunidad */}
       <Route path="/community" element={<CommunityPage />} />
 
@@ -64,8 +50,12 @@ export const RouterApp = () => {
       <Route path="/profile/edit" element={<ProfileEditPage />} />
       <Route path="/profile/admin" element={<ProfileAdminPage />} />
 
+       {/* Rutas del modulo de monitoreo */}
+      <Route path="/monitoring" element={<MonitoringPage />} />
+      <Route path="/monitoring/history" element={<DataHistoryPage />} />
+      <Route path="/monitoring/alerts" element={<AlertsPage />} />
+      <Route path="/monitoring/real-time" element={<RealTimeMonitoringPage />} />
      
-
       {/* Rutas protegidas: */}
       <Route element={<ProtectedRoutes />} >
         {/* Aqui van las rutas protegidas */}
