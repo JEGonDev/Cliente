7d9ceb1f6f5863cf92fb8311bc506b16f8d1825d
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2025-05-19 12:30:17 -0500

    docs(education): Se implementa documentacion para el componente UI AdminControlPanel y ArticleFormModal

--- src/features/education/ui/AdminControlPanel.jsx
+++ src/features/education/ui/AdminControlPanel.jsx
@@ -1,6 +1,16 @@
 import PropTypes from 'prop-types';
 import { Button } from '../../../ui/components/Button';
 
+/**
+ * Panel de control administrativo para módulos educativos
+ * 
+ * Este componente muestra tres botones para crear, modificar o eliminar un módulo.
+ * 
+ * @param {Object} props
+ * @param {Function} props.onCreateClick - Función que se ejecuta al hacer clic en "Crear módulo"
+ * @param {Function} props.onEditClick - Función que se ejecuta al hacer clic en "Modificar módulo"
+ * @param {Function} props.onDeleteClick - Función que se ejecuta al hacer clic en "Eliminar módulo"
+ */
 export const AdminControlPanel = ({
   onCreateClick,
   onEditClick,
